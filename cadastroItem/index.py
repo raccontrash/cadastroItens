68bde9e2e84f610b1cdfa97d99969d1aee0cd79c
raccontrash <moreirabrun0@protonmail.com>
2023-01-20 10:55:10 -0300

    cadastroIten.PY

---
+++ cadastroItem/index.py
@@ -0,0 +1,9 @@
+# INDEX.PY TELA PRINCIPAL
+print("Versão x.x.xx")
+print("[1]. Cadastrar novo item.")
+print("2. Visualizar itens.")
+print("3. Sair.")
+
+i = input("Qual menu você deseja acessar?")
+if i == "1":
+    a = input("digite a qual tipo de gêneros deseja acessar:")
