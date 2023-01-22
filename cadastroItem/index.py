55649b7bad42eaafb5315581dddffb75c430b469
raccontrash <moreirabrun0@protonmail.com>
2023-01-21 22:08:56 -0300

    OBJ

--- cadastroItem/index.py
+++ cadastroItem/index.py
@@ -7,3 +7,5 @@ print("3. Sair.")
 i = input("Qual menu você deseja acessar?")
 if i == "1":
     a = input("digite a qual tipo de gêneros deseja acessar:")
+if i == "2":
+    print()
---
+++ cadastroItem/listaItens.py
@@ -0,0 +1,14 @@
+# ARMAZENAMENTO DE OBJETOS AQUI
+#
+#
+# EM CASO DE DÚVIDAS, CONSULTAR TABELA DE ORGANIZAÇÃO DE GENEROS.
+generoLista1 = []
+generoLista2 = []
+Item = {
+    'nomeItem' : 'maca',
+    'varianteItem' : 'fugi',
+    'catmat' : 106324,
+    'tipoGenero' : (generoLista1)
+}
+
+print(Item) # CERTIFICANDO QUE TÁ TUDO CERTO C OBJETO
